a2cf899dd096af67dc9b1b4b85901c241e476519
Colin Fausnaught <cfausn@gmail.com>
2023-01-16 20:50:43 -0500

    Added pagination, bug with borders

--- front/src/components/Breeds.js
+++ front/src/components/Breeds.js
@@ -5,35 +5,36 @@ import axios from 'axios';
 function Breeds() {
   const [breeds, setBreeds] = useState([]);
   const [searchText, setSearchText] = useState('');
-  const [displayedBreeds, setDisplayedBreeds] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [entriesPerPage] = useState(10);
 
   useEffect(() => {
     async function fetchData() {
       const result = await axios.get('http://localhost:5000/breeds');
+      console.log("result", JSON.stringify(result.data))
       setBreeds(result.data);
-      setDisplayedBreeds(result.data);
     }
     fetchData();
   }, []);
 
-  useEffect(() => {
-    setDisplayedBreeds(
-      Object.entries(breeds).filter(
-        ([breed, subBreeds]) =>
-          breed.toLowerCase().includes(searchText.toLowerCase()) ||
-          subBreeds.some(subBreed =>
-            subBreed.toLowerCase().includes(searchText.toLowerCase())
-          )
-      )
-    );
-  }, [searchText, breeds]);
+  const filteredBreeds = Object.entries(breeds).filter(([breed, subBreeds]) =>
+    breed.toLowerCase().includes(searchText.toLowerCase()) ||
+    subBreeds.some(subBreed => subBreed.toLowerCase().includes(searchText.toLowerCase()))
+  );
 
+  const indexOfLastEntry = currentPage * entriesPerPage;
+  const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
+  const currentEntries = filteredBreeds.slice(indexOfFirstEntry, indexOfLastEntry);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
   
   return (
     <div className="container">
       <div className="search-container">
         <label>
-          Search by Breed:<br/>
+          Search by Breed:
           <input
             type="text"
             value={searchText}
@@ -41,41 +42,39 @@ function Breeds() {
           />
         </label>
       </div>
+      <div className="pagination">
+        {Array.from({ length: Math.ceil(filteredBreeds.length / entriesPerPage) }, (_, i) => (
+          <span key={i} onClick={() => paginate(i + 1)} className={`page-number ${currentPage === i + 1 ? 'active' : ''}`}>
+            {i + 1}
+          </span>
+        ))}
+      </div>
       <table className="table">
         <thead>
           <tr>
-            <th className="table-header">Breed</th>
-            <th className="table-header">SubBreed</th>
-            <th className="table-header">Image</th>
+            <th className="table-header" style={{borderBottom: '2px solid black'}}>Breed</th>
+            <th className="table-header" style={{borderBottom: '2px solid black'}}>SubBreed</th>
+            <th className="table-header" style={{borderBottom: '2px solid black'}}>Image</th>
           </tr>
         </thead>
         <tbody>
-          {Object.entries(breeds)
-            .filter(
-              ([breed, subBreeds]) =>
-                breed.toLowerCase().includes(searchText.toLowerCase()) ||
-                subBreeds.some(subBreed =>
-                  subBreed.toLowerCase().includes(searchText.toLowerCase())
-                )
-            )
-            .slice(0, 10)
-            .map(([breed, subBreeds]) => (
-              <React.Fragment key={breed}>
-                {subBreeds.map(subBreed => (
-                  <tr key={subBreed}>
-                    <td className="table-data">{breed}</td>
-                    <td className="table-data">{subBreed}</td>
-                    <td className="table-data">
-                      <img
-                        className="image"
-                        src={`http://localhost:5000/image/${breed}/${subBreed}`}
-                        alt={`${breed} ${subBreed}`}
-                      />
-                    </td>
-                  </tr>
-                ))}
-              </React.Fragment>
-            ))}
+          {currentEntries.map(([breed, subBreeds]) => (
+            <React.Fragment key={breed}>
+              {subBreeds.map((subBreed, index) => (
+                <tr key={subBreed} style={index === 0 ? {borderTop: '2px solid black'} : {}}>
+                  {index === 0 ? <td className="table-data">{breed}</td> : <td className="table-data"></td>}
+                  <td className="table-data">{subBreed}</td>
+                  <td className="table-data">
+                    <img
+                      className="image"
+                      src={`http://localhost:5000/image/${breed}/${subBreed}`}
+                      alt={`${breed} ${subBreed}`}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </React.Fragment>
+          ))}
         </tbody>
       </table>
     </div>
