00a35dbc615070063658b8d00cdda36d34cc2df2
Colin Fausnaught <cfausn@gmail.com>
2023-01-17 00:13:35 -0500

    Added documentation, added sorting, fixed breeds per page, and finalized backend code

--- back/server.py
+++ back/server.py
@@ -2,14 +2,16 @@ from flask import Flask, jsonify, redirect
 import requests
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
-
+# The DogBreedsAPI class is used to interact with the public dog breeds API
+# It has two main methods:
+# 1. get_breeds_list() - Retrieves a list of all dog breeds and their variants
+# 2. get_random_image(breed, subBreed) - Retrieves a random image of a specific breed and variant
 class DogBreedsAPI:
     def __init__(self):
         self.url = 'https://dog.ceo/api/breeds/list/all'
         self.img_url = 'https://dog.ceo/api/breed/{}/{}/images/random'
 
+    # Retrieves a list of all dog breeds and their variants
     def get_breeds_list(self):
         response = requests.get(self.url)
         data = response.json()
@@ -17,11 +19,7 @@ class DogBreedsAPI:
         breeds_with_variants = {breed: variants for breed, variants in breeds.items() if len(variants) > 0}
         return breeds_with_variants
 
-    def get_variants_count(self):
-        breeds_with_variants = self.get_breeds_list()
-        variants_count = {breed: len(variants) for breed, variants in breeds_with_variants.items()}
-        return variants_count
-
+    # Retrieves a random image of a specific breed and variant
     def get_random_image(self, breed, subBreed):
         url = self.img_url.format(breed, subBreed)
         response = requests.get(url)
@@ -29,18 +27,20 @@ class DogBreedsAPI:
         image_url = data['message']
         return image_url
 
+# Create an instance of the DogBreedsAPI class
 api = DogBreedsAPI()
 
+# Initialize Flask app and enable CORS
+app = Flask(__name__)
+CORS(app)
+
+# Endpoint to retrieve a list of all dog breeds and their variants
 @app.route('/breeds', methods=['GET'])
 def get_breeds():
     breeds_with_variants = api.get_breeds_list()
     return jsonify(breeds_with_variants)
 
-@app.route('/variantsCount', methods=['GET'])
-def get_variants_count():
-    variants_count = api.get_variants_count()
-    return jsonify(variants_count)
-
+# Endpoint to retrieve a random image of a specific breed and variant
 @app.route('/image/<breed>/<subBreed>', methods=['GET'])
 def get_image(breed, subBreed):
     image_url = api.get_random_image(breed, subBreed)
--- front/src/components/Breeds.js
+++ front/src/components/Breeds.js
@@ -1,35 +1,61 @@
-// In the frontend folder, create a new file called `src/App.js`
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import axios from 'axios';
 
 function Breeds() {
-  const [breeds, setBreeds] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
-  const [entriesPerPage] = useState(10);
+  const [breeds, setBreeds] = useState([]); 
+  const [searchText, setSearchText] = useState(''); 
+  const [currentPage, setCurrentPage] = useState(1); 
+  const [breedsPerPage, setBreedsPerPage] = useState(10); 
+  const [sortOrder, setSortOrder] = useState({ breed: 'asc', variant: 'asc', number: 'asc', image: 'asc' }); // added state to store the sort order of each column
+  const [sortBy, setSortBy] = useState('breed'); // added state to store the column to sort by
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios.get('http://localhost:5000/breeds');
-      console.log("result", JSON.stringify(result.data))
-      setBreeds(result.data);
+      const result = await axios.get('http://localhost:5000/breeds'); // Making a GET request to the '/breeds' endpoint on the flask backend
+      setBreeds(result.data); // Setting the fetched data to the 'breeds' state
     }
-    fetchData();
+    fetchData(); // Calling the function to fetch the data on component mount
   }, []);
 
-  const filteredBreeds = Object.entries(breeds).filter(([breed, subBreeds]) =>
+  // added function to handle sorting
+  const handleSort = (column) => {  
+    if (sortBy === column) {
+      setSortOrder({ ...sortOrder, [column]: sortOrder[column] === 'asc' ? 'desc' : 'asc' });
+    } else {
+      setSortBy(column);
+      setSortOrder({ breed: 'asc', variant: 'asc', number: 'asc', image: 'asc', [column]: 'asc' });
+    }
+  }
+
+  const filteredBreeds = Object.entries(breeds).filter(([breed, subBreeds]) => // Filtering the breeds data based on the search text input
     breed.toLowerCase().includes(searchText.toLowerCase()) ||
     subBreeds.some(subBreed => subBreed.toLowerCase().includes(searchText.toLowerCase()))
   );
 
-  const indexOfLastEntry = currentPage * entriesPerPage;
-  const indexOfFirstEntry = indexOfLastEntry - entriesPerPage;
-  const currentEntries = filteredBreeds.slice(indexOfFirstEntry, indexOfLastEntry);
+  // added sorting logic
+  const sortedBreeds = filteredBreeds.sort((a, b) => {
+    let sortVal = 0;
+    if (sortBy === 'breed') {
+      sortVal = a[0].localeCompare(b[0]);
+    } else if (sortBy === 'variant') {
+      sortVal = a[1][0].localeCompare(b[1][0]);
+    } else if (sortBy === 'number') {
+      sortVal = a[1].length - b[1].length;
+    } else if (sortBy === 'image') {
+      sortVal = a[1][0].localeCompare(b[1][0]);
+    }
+    if (sortOrder[sortBy] === 'desc') {
+      sortVal *= -1;
+    }
+    return sortVal;
+  });
 
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  }
-  
+  const indexOfLastEntry = currentPage * breedsPerPage; // Calculating the index of the last entry to display on the current page
+  const indexOfFirstEntry = indexOfLastEntry - breedsPerPage; // Calculating the index of the first entry to display on the current page
+  const currentEntries = filteredBreeds.slice(indexOfFirstEntry, indexOfLastEntry); // Getting the current entries to display
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber); // Function to update the current page number
+
   return (
     <div className="container">
       <div className="search-container">
@@ -42,8 +68,18 @@ function Breeds() {
           />
         </label>
       </div>
+      <div className="breeds-per-page">
+        <label>
+          Breeds Per Page
+          <input
+            type="number"
+            value={breedsPerPage}
+            onChange={e => {e.target.value >= 1 ? setBreedsPerPage(e.target.value) : setBreedsPerPage(1)}}
+          />
+        </label>
+      </div>
       <div className="pagination">
-        {Array.from({ length: Math.ceil(filteredBreeds.length / entriesPerPage) }, (_, i) => (
+        {Array.from({ length: Math.ceil(filteredBreeds.length / breedsPerPage) }, (_, i) => (
           <span key={i} onClick={() => paginate(i + 1)} className={`page-number ${currentPage === i + 1 ? 'active' : ''}`}>
             {i + 1}
           </span>
@@ -52,24 +88,22 @@ function Breeds() {
       <table className="table">
         <thead>
           <tr>
-            <th className="table-header" style={{borderBottom: '2px solid black'}}>Breed</th>
-            <th className="table-header" style={{borderBottom: '2px solid black'}}>SubBreed</th>
-            <th className="table-header" style={{borderBottom: '2px solid black'}}>Image</th>
+                <th className="table-header" style={{borderBottom: '2px solid black'}} onClick={() => handleSort('breed')}>Breed</th>
+                <th className="table-header" style={{borderBottom: '2px solid black'}} onClick={() => handleSort('variant')}>Variant</th>
+                <th className="table-header" style={{borderBottom: '2px solid black'}} onClick={() => handleSort('number')}>Number of Variants</th>
+                <th className="table-header" style={{borderBottom: '2px solid black'}} onClick={() => handleSort('image')}>Image</th>
           </tr>
         </thead>
         <tbody>
           {currentEntries.map(([breed, subBreeds]) => (
             <React.Fragment key={breed}>
               {subBreeds.map((subBreed, index) => (
-                <tr key={subBreed} style={index === 0 ? {borderTop: '2px solid black'} : {}}>
+                <tr key={subBreed} style={index === 0 ? {backgroundColor: '#F5F5DC'} : {backgroundColor: '#D8D8A9'}}>
                   {index === 0 ? <td className="table-data">{breed}</td> : <td className="table-data"></td>}
                   <td className="table-data">{subBreed}</td>
+                  {index === 0 ? <td className="table-data">{subBreeds.length}</td> : <td className="table-data"></td>}
                   <td className="table-data">
-                    <img
-                      className="image"
-                      src={`http://localhost:5000/image/${breed}/${subBreed}`}
-                      alt={`${breed} ${subBreed}`}
-                    />
+                    <img className="image" src={`http://localhost:5000/image/${breed}/${subBreed}`} alt={`${breed} ${subBreed}`} />
                   </td>
                 </tr>
               ))}
@@ -79,6 +113,7 @@ function Breeds() {
       </table>
     </div>
   );
+
 }
 
 export default Breeds;
